11a3f39c07ead4209fe84e7c3e6851600182e99e
jeremiah victor <jeremiahv791@gmail.com>
2024-11-13 13:05:23 +0100

    first commit

---
+++ src/anotherMain.rs
@@ -0,0 +1,5 @@
+fn run() {
+    println!("Hello!");
+    let x = 5 + /* 90 + */ 5;
+    println!("Is `x` 10 or 100? x = {}", x);
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    let  x = 5;
+    println!("x is = {}", x);
+    let y = 10;
+    println!("x + y is = {}", x + y);
+}
