731683b66b7e34bcf68bf333633e5ad50b3103a3
jeremiah victor <jeremiahv791@gmail.com>
2024-11-17 10:33:23 +0100

    got better understanding of tuple

--- src/work.rs
+++ src/work.rs
@@ -8,10 +8,12 @@ pub fn greeting() {
     //tuples
     let tups: (i32, bool, char) = (10, true, 'A');
     let tups2: (i64, bool, &str) = (20, false, "hello");
+    let tups3: (i64, bool, &str) = (20, false, "King");
 
     // Print elements of tups
     println!("tups: ({}, {}, '{}')", tups.0, tups.1, tups.2);
     // Print elements of tups2
     println!("tups2: ({}, {}, \"{}\")", tups2.0, tups2.1, tups2.2);
+    println!("tups3: {:?}", tups3);
 
 }
